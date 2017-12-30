ecc4da2b4870d207bb38bd860023701c92d2764a
Allyson Wesman <allysonwesman@gmail.com>
2017-12-30 23:03:57 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('#typing').on("keydown", function (key) {
+  $('form').on("keydown", function (key) {
     if (key.which === 71) {
       alert ("You have pressed G");
     }
