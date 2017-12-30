4fd9dfdb47731e5f90c645cbd2dc6425f5494a52
Allyson Wesman <allysonwesman@gmail.com>
2017-12-30 23:01:33 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,21 @@
-//define functions here
+function getIt() {
+  $('p').on("click", function () {
+    alert ("Hey!");
+  })
+  return;
+}
+
+function frameIt() {
+  $(window).on("load", function () {
+    $('img').addClass("tasty")
+  })
+  return;
+}
+
 
 $(document).ready(function(){
 
-// call functions here
+frameIt();
+getIt();
 
 });
